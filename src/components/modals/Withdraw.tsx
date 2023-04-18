5ab110b019becf7792dc0fd09a0947a6412deb7e
Yuriy Berland <berland.um@yandex.ru>
2023-04-18 13:07:15 +0300

    Unit tests

--- src/components/modals/Withdraw.tsx
+++ src/components/modals/Withdraw.tsx
@@ -61,12 +61,14 @@ const WithdrawModal = () => {
       dispatch(actions.setTxPending(true))
       CloseWindow()
 
+      console.log(RqBody)
+
       const WithdrawResponse = await fetch(withdrawUrl, {
+         method: "POST",
          headers: {
            'Accept': 'application/json',
            'Content-Type': 'application/json'
          },
-         method: "POST",
          body: JSON.stringify(RqBody)
         })
 
